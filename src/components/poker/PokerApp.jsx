6da94c20ebe2d1d7ad70f94db2c3654cb9596dff
abdallahsafa <abdallah.safa@vanderbilt.edu>
2024-07-04 16:07:03 -0400

    header,footer, and start list page

--- src/components/poker/PokerApp.jsx
+++ src/components/poker/PokerApp.jsx
@@ -1,17 +1,23 @@
 import './PokerApp.css';
 import { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route, BrowserRouter, useNavigate} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, BrowserRouter, useNavigate, useParams, Link,useLocation} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function PokerApp() {
     return (
         <div className="PokerApp">
             <BrowserRouter>
+                <HeaderComponent/>
                 <Routes>
                     <Route path="/" element={<LoginComponent />} />
                     <Route path="/login" element={<LoginComponent />} />
-                    <Route path="/welcome" element={<WelcomeComponent />} />
+                    <Route path="/welcome/:username" element={<WelcomeComponent />} />
+                    <Route path='*' element={<Error />} />
+                    <Route path="/games" element={<ListPokerGameComponent />} />
+                    <Route path="/logout" element={<LogoutComponent />} />
                 </Routes>
             </BrowserRouter>
+            <FooterComponent/>
         </div>
     );
 }
@@ -34,9 +40,10 @@ function LoginComponent() {
         // Replace this with your actual login logic
         if (username === 'user' && password === 'pass') {
             setLoginStatus('success');
-            nav('/welcome')
+            nav(`/welcome/${username}`)
         } else {
             setLoginStatus('fail');
+            nav('error')
         }
     }
 
@@ -74,9 +81,123 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+    const {username} = useParams();
+
     return (
         <div className="Welcome">
-            Welcome Component
+            <h1>Welcome {username} !</h1>
+            <div>
+                Welcome Component
+                Your games are <Link to="/games">here</Link>
+            </div>
         </div>
     );
 }
+
+function Error() {
+    return (
+        <div className="error">
+            <h1>
+                Sorry! Page not found.
+            </h1>
+            <div>
+                <a href="/login">Go to Login</a>
+            </div>
+        </div>
+    );
+}
+
+function ListPokerGameComponent() {
+    const game = {id:1,buyin:10, endNight: 20, netNight: 10}
+
+    return (
+        <div className="container">
+            <h1>Poker Games</h1>
+            <div className= "table">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>id</td>
+                            <td>buy in</td>
+                            <td>end night</td>
+                            <td>net night</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{game.id}</td>
+                            <td>{game.buyin}</td>
+                            <td>{game.endNight}</td>
+                            <td>{game.netNight}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+
+function HeaderComponent() {
+    //const location = useLocation();
+
+    // const getNavbarClass = () => {
+    //     switch (location.pathname) {
+    //         case '/welcome':
+    //             return "border-bottom border-light border-3  p-2 ";
+    //         case '/games':
+    //             return 'navbar navbar-expand-lg navbar-light bg-success fixed-top';
+    //         case '/login':
+    //             return "border-bottom border-light border-3 bgp-2 ";
+    //         case '/logout':
+    //             return 'navbar navbar-expand-lg navbar-light bg-danger fixed-top';
+    //         default:
+    //             return 'navbar navbar-expand-lg navbar-light bg-dark fixed-top';
+    //     }
+    // };
+
+    //<header className="border-bottom border-light border-3  p-2 ">
+
+        return (
+        <header className= "border-bottom border-light bg-light border-3  p-2">
+            <div className="container">
+            <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black"
+                           href="https://www.pokes.com">Poker</a>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link"
+                                                                    to="/welcome/user">Home</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/games">Poker Games</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        );
+        }
+
+
+        function FooterComponent() {
+        return (
+        <div className="footer">
+        <h1>Footer</h1>
+        </div>
+        );
+    }
+
+        function LogoutComponent() {
+        return (
+        <div className="LogoutForm">
+        <h1>You are logged out!</h1>
+        <div>thank you !</div>
+        </div>
+        );
+    }
